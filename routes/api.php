5e692026cd946a89ad30dc1fc21ae7f72856c739
lalolito <143973469+lalolito@users.noreply.github.com>
2024-11-28 14:11:40 -0500

    aqui quiero conectar mi api.php con mis controladores y agrege un middleware de autenticación:

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,33 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CitasController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/citas', [CitasController::class, 'index']);
+    Route::get('/cita/{id}', [CitasController::class, 'show']);
+    Route::post('/cita', [CitasController::class, 'store']);
+    Route::put('/cita/{id}', [CitasController::class, 'update']);
+    Route::delete('/cita/{id}', [CitasController::class, 'destroy']);
+}); 
+
+/*Route::get('/cita', function () {
+    return'cliente lista';
+});
+
+Route::get('/cliente/{id}', function () {
+    return 'un cliente   ';
+});
+
+Route::post('/cliente', function () {
+    return 'creando un cliente';
+});
+
+Route::put('/cliente/{id}', function () {
+    return 'actualizando uncliente ';
+});
+ 
+Route::delete('/cliente/{id}', function () {
+    return 'eliminando cliente ';
+});*/
+
